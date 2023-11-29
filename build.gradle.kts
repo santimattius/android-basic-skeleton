9a529048d34a35f4138098ecf4b920762a7a9d77
Santiago Mattiauda <santimattius@gmail.com>
2023-11-29 08:29:07 -0300

    Update and migrations (#1)
    * [update] update project
    
    * [update] project versions

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.detekt) apply false
+}
+true
